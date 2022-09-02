6ef5cf6aa9c782e4a7d4e2f35c901baab9735f10
armstjc <70788681+armstjc@users.noreply.github.com>
2022-09-02 15:05:52 -0400

    All play-by-play game logs are downloaded.
    Be warned, this update is massive.

--- get_stats.r
+++ get_stats.r
@@ -3,26 +3,28 @@ library(dplyr)
 library(glue)
 library(dplyr)
 
-seasons = 2019:2022
+seasons = 2022:2022
 
 for (s in seasons) {
   print(glue::glue("Donwloading all the play-by-play data for the NCAA Baseball season of {s}"))
   df = read.csv(glue::glue("{s}_batting_01.csv"))
   df2 = read.csv(glue::glue("{s}_batting_02.csv"))
   main_df <- union(df,df2)
-  list = main_df$game_id
+  game_list = main_df$game_id
   #print(list)
-  list = unique(list)
-  list_len = length(list)
+  game_list = unique(game_list)
+  game_list_len = length(game_list)
   count = 0
   
-  l_range = 837:8406
+  l_range = 2660:8299
   
-  for (i in list){
+  for (i in l_range){
     count = count + 1
-    print(glue::glue("{count}/{list_len}: {i}"))
-    filename <- glue::glue("pbp/{i}.csv")
-    stats <- ncaa_baseball_pbp(glue::glue('https://stats.ncaa.org/game/play_by_play/{i}'))
+    
+    gid = game_list[i]
+    print(glue::glue("{i}/{game_list_len}: {gid}"))
+    filename <- glue::glue("pbp/{gid}.csv")
+    stats <- ncaa_baseball_pbp(glue::glue('https://stats.ncaa.org/game/play_by_play/{gid}'))
     write.csv(stats,filename,row.names=FALSE)
     Sys.sleep(4)
   }
